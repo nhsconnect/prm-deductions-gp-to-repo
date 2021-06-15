f50ccd28e5f7e60e26c354edab686a83fa364c1a
Oktawia Kata <oktawia.kata1@nhs.net>
2021-06-15 16:02:40 +0100

    [PRMT-2024] Added ingress rule for gp-to-repo to reach ehr-repo

--- terraform/alb-internal.tf
+++ terraform/alb-internal.tf
@@ -69,3 +69,17 @@ resource "aws_lb_listener_certificate" "gp-to-repo-int-listener-cert" {
   listener_arn    = data.aws_ssm_parameter.int-alb-listener-https-arn.value
   certificate_arn = aws_acm_certificate_validation.gp-to-repo-cert-validation.certificate_arn
 }
+
+
+data "aws_ssm_parameter" "service-to-ehr-repo-sg-id" {
+  name = "/repo/${var.environment}/output/prm-deductions-ehr-repository/service-to-ehr-repo-sg-id"
+}
+
+resource "aws_security_group_rule" "gp-to-repo-to-ehr-repo" {
+  type = "ingress"
+  protocol = "TCP"
+  from_port = 443
+  to_port = 443
+  security_group_id = data.aws_ssm_parameter.service-to-ehr-repo-sg-id.value
+  source_security_group_id = data.aws_ssm_parameter.deductions_private_gp_to_repo_sg_id.value
+}
