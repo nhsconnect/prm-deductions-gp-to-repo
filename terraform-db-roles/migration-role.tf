22a317430008e262aacb7491a8afb737d920a390
Jack Slater <jslater@thoughtworks.com>
2021-07-01 15:32:11 +0100

    [PRMT-1733] add postgres script to set correct table permissions for db users

--- terraform-db-roles/migration-role.tf
+++ terraform-db-roles/migration-role.tf
@@ -10,14 +10,7 @@ resource "postgresql_grant" "migration_role_schema_usage_grant" {
   privileges  = ["USAGE", "CREATE"]
 }
 
-# Giving access to all tables instead of SequelizeMeta until this is implemented: https://github.com/cyrilgdn/terraform-provider-postgresql/pull/79
-resource "postgresql_grant" "migration_role_table_read_write_grant" {
-  database    = var.db_name
-  role        = postgresql_role.migration_role.name
-  schema      = "public"
-  object_type = "table"
-  privileges  = ["SELECT", "INSERT"]
-}
+# Table grant permissions handled by ./tasks grant_db_permissions
 
 resource "postgresql_role" "migration_user" {
   name     = "migration_user"
