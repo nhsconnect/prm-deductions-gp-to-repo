ba6057504db5960eeb188e09e587b19e8748828b
Rosie Amphlett <rosie.amphlett1@nhs.net>
2021-10-26 17:24:53 +0100

    [PRMT-2384] Closed access to ECS and ALBs after pen-testing

--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -1,7 +1,7 @@
 locals {
   ecs_cluster_id  = aws_ecs_cluster.ecs-cluster.id
   ecs_task_sg_id = aws_security_group.ecs-tasks-sg.id
-  ecs_task_sg_ids = var.allow_vpn_to_ecs_tasks ? [aws_security_group.ecs-tasks-sg.id, aws_security_group.vpn_to_gp_to_repo_ecs.id] : [aws_security_group.ecs-tasks-sg.id]
+  ecs_task_sg_ids = var.allow_vpn_to_ecs_tasks ? [aws_security_group.ecs-tasks-sg.id, aws_security_group.vpn_to_gp_to_repo_ecs[0].id] : [aws_security_group.ecs-tasks-sg.id]
   private_subnets = split(",", data.aws_ssm_parameter.deductions_private_private_subnets.value)
   # alb_tg_arn      = aws_alb_target_group.alb-tg.arn
   int_alb_tg_arn      = aws_alb_target_group.internal-alb-tg.arn
